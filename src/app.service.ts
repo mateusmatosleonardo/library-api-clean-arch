8da4acfab1a37f9912f244a157e38ad22841cd85
Mateus Leonardo <m4teusmatos@gmail.com>
2026-05-12 22:52:28 -0300

    refactor: integrate UserModule into app module and clean up AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,8 @@
 import { Injectable } from "@nestjs/common";
-import { UserEntity } from "@/user/domain/entities/user.entity";
 
 @Injectable()
 export class AppService {
-  private user = new UserEntity({
-    name: "John Doe",
-    email: "john.doe@example.com"
-  });
-
   getHello(): string {
-    console.log(this.user.toJSON());
     return "Hello World!";
   }
 }
