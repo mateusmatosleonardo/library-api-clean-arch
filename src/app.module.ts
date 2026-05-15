0a813ef2498f02b3381cf1dc364af6a1be4a2ef4
Mateus Leonardo <m4teusmatos@gmail.com>
2026-05-14 23:08:28 -0300

    feat: add DatabaseModule to app module imports for database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from "./app.service";
 import { UserModule } from "./user/infrastructure/user.module";
 import { ConfigModule } from "@nestjs/config";
 import { EnvConfigModule } from "./shared/infrastructure/env-config/env-config.module";
+import { DatabaseModule } from './shared/infrastructure/database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
-    EnvConfigModule
+    EnvConfigModule,
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService]
