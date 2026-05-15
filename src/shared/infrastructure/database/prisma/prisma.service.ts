eac0dda8257406b764268f565dadb33567e035c4
Mateus Leonardo <m4teusmatos@gmail.com>
2026-05-14 23:34:24 -0300

    feat: enhance PrismaService with database connection management and error handling

--- src/shared/infrastructure/database/prisma/prisma.service.ts
+++ src/shared/infrastructure/database/prisma/prisma.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private readonly configService: ConfigService) {
+    const databaseUrl = configService.get<string>("DATABASE_URL");
+
+    if (!databaseUrl) {
+      throw new Error("DATABASE_URL não foi definida");
+    }
+
+    super({
+      adapter: new PrismaPg({
+        connectionString: databaseUrl
+      })
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
