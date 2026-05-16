3e7c2296bc1d29abce4987cd39e8717f2d6211a8
Mateus Leonardo <m4teusmatos@gmail.com>
2026-05-15 21:38:09 -0300

    chore: update ESLint configuration to include only-throw-error rule and modify repository contract to allow null return for findById

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,7 +32,8 @@ export default tseslint.config(
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-unsafe-return": "off",
       "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off"
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/only-throw-error": "off"
     }
   },
   eslintConfigPrettier
