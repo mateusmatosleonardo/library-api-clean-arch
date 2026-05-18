1f35409d28ab5f738e99774c929f9cd77c75fac4
Mateus Leonardo <m4teusmatos@gmail.com>
2026-05-18 09:35:26 -0300

    feat: enhance SignUpUseCase to validate input fields and handle conflicts with custom exceptions

--- src/user/application/use-cases/signup.use-case.ts
+++ src/user/application/use-cases/signup.use-case.ts
@@ -1,5 +1,7 @@
 import { HashProvider } from "@/shared/application/providers/hash-provider";
 import { UseCase as DefaultUseCase } from "@/shared/application/use-cases/use-case";
+import { ConflictException } from "@/shared/application/errors/http-exceptions/conflict.exception";
+import { BadRequestException } from "@/shared/application/errors/http-exceptions/bad-request.exception";
 import { UserEntity } from "@/user/domain/entities/user.entity";
 import { UserRepository } from "@/user/domain/repositories/user.respository";
 
@@ -19,8 +21,8 @@ export namespace SignUpUseCase {
     phone: string;
     cpf: string;
     password: string;
-    createdAt: Date;
-    updatedAt: Date;
+    created_at: Date;
+    updated_at: Date;
   };
 
   export class UseCase implements DefaultUseCase<Input, Output> {
@@ -30,38 +32,54 @@ export namespace SignUpUseCase {
     ) {}
 
     async execute(input: Input): Promise<Output> {
-      const { name, email, phone, cpf, password } = input;
+      const requiredFields: (keyof Input)[] = [
+        "name",
+        "email",
+        "phone",
+        "cpf",
+        "password"
+      ];
 
-      if (!name) {
-        throw new Error("Name is required");
+      const fields: Record<string, string> = {};
+
+      requiredFields.forEach((field) => {
+        if (!input[field]) {
+          fields[field] = "Field required";
+        }
+      });
+
+      if (Object.keys(fields).length > 0) {
+        throw new BadRequestException("Input data not provided", fields);
       }
 
-      if (!email) {
-        throw new Error("Email is required");
+      const [emailExists, phoneExists, cpfExists] = await Promise.all([
+        this.userRepository.findByEmail(input.email),
+        this.userRepository.findByPhone(input.phone),
+        this.userRepository.findByCpf(input.cpf)
+      ]);
+
+      if (emailExists) {
+        throw new ConflictException("Email already in use");
       }
 
-      if (!phone) {
-        throw new Error("Phone is required");
+      if (phoneExists) {
+        throw new ConflictException("Phone already in use");
       }
 
-      if (!cpf) {
-        throw new Error("CPF is required");
+      if (cpfExists) {
+        throw new ConflictException("Cpf already in use");
       }
 
-      if (!password) {
-        throw new Error("Password is required");
-      }
-
-      const hashPassword = await this.hashProvider.hash(password);
+      const hashPassword = await this.hashProvider.hash(input.password);
 
       const entity = new UserEntity({
         ...input,
         password: hashPassword
       });
 
-      await this.userRepository.insert(entity);
+      const user = await this.userRepository.insert(entity);
 
-      return entity.toJSON();
+      return user.toJSON();
     }
   }
 }
