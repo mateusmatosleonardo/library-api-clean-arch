f55806edf7048e748c280c20812cd6464c786314
Mateus Leonardo <m4teusmatos@gmail.com>
2026-05-18 09:35:44 -0300

    feat: add global validation pipe to enforce input validation and transformation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { ApplicationExceptionFilter } from "./shared/infrastructure/http/application-exception.filter";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
   app.useGlobalFilters(new ApplicationExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
